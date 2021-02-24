6bd3508d26580c46dc9eeceb0ca69e31abfe222a
Kostya Rybka <itenn08@gmail.com>
2021-02-24 13:00:24 +0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -5,6 +6,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+>>>>>>> 6a0511f (Initialize project using Create React App)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
